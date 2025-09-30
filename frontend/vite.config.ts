94457eff838609689b5908972a8b31d5819fb11b
ncuMercy <114423019@cc.ncu.edu.tw>
2025-09-30 01:28:25 +0000

    chore: change baseurl to env

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,26 +1,30 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
-import path, { dirname }  from 'path';
+import path, { dirname } from 'path';
 import { fileURLToPath } from 'url';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 // https://vite.dev/config/
-export default defineConfig({
-  base:'/task-manage/',
-  plugins: [react()],
-  server: {
-    host: '0.0.0.0',
-    port: 3000,
-    strictPort: true,
-    watch: {
-      usePolling: true // ensures file changes in mounted volumes are detected
-    }
-  },
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(`${process.cwd()}/.env`, '')
+
+  return {
+    base: env.VITE_BUCKET_NAME,
+    plugins: [react()],
+    server: {
+      host: '0.0.0.0',
+      port: 3000,
+      strictPort: true,
+      watch: {
+        usePolling: true // ensures file changes in mounted volumes are detected
+      }
+    },
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, './src'),
+      }
     }
   }
 })
